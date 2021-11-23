7abaceb1164c53fcdc8eada46f4d1ab16b7b1b9f
Gamester1128 <77338063+Gamester1128@users.noreply.github.com>
2021-11-23 22:15:59 +0000

    Initial tutorial setup for discord bot

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('Real Gamester1128 has spawned in your world!');
+});
+
+// Get token and login
+const { token } = require('./config.json');
+client.login(token);
