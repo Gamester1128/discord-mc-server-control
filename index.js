039a723a1df88f2f37a462f53183a42d1dbaf9c9
Gamester1128 <77338063+Gamester1128@users.noreply.github.com>
2021-12-01 23:34:09 +0000

    build.xml now builds fake mc jar

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
     console.log('Real Gamester1128 has spawned in your world!');
+    
 });
+
+client.on()
 
 // Get token and login
 const { token } = require('./config.json');
---
+++ src/testing/FakeMCServer.java
@@ -0,0 +1,31 @@
+package testing;
+
+import java.util.Scanner;
+
+public class FakeMCServer {
+
+    private static final int sleep = 2000;
+
+    private static Runnable inputThread = () -> {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println("[CANT BE ASKED FOR TIME] [FAKEMINECRAFTSERVER/ERROR] : no commands exist! input sent: " + input);
+        }
+    };
+
+    public static void main(String[] args) {
+        new Thread(inputThread, "Input Thread").start();
+
+        int i = 0;
+        while(true) {
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("[69:69:69] [FAKEMINECRAFT/INFO] : Hehe counting hehe, now it is " + i++);
+        }
+    }
+
+}
